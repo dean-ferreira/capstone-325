89ce4c0a1c6471349ab31cb1659929e8de3c17fd
dean-ferreira <deanferreira20@gmail.com>
2024-03-20 01:10:18 -0500

    Add endpoint to sort states by average price

--- server/routes/states.js
+++ server/routes/states.js
@@ -12,4 +12,28 @@ router.get('/', async (req, res) => {
     }
 });
 
+// Get x number of states with the lowest average price
+router.get('/lowest/:num', async (req, res) => {
+    try {
+        const states = await State.find()
+            .sort({ average: 1 })
+            .limit(parseInt(req.params.num));
+        res.json(states);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Get x number of states with the highest average price
+router.get('/highest/:num', async (req, res) => {
+    try {
+        const states = await State.find()
+            .sort({ average: -1 })
+            .limit(parseInt(req.params.num));
+        res.json(states);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 module.exports = router;
