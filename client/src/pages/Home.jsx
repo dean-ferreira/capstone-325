6b7f794831feff37668a1919b9eabecfc7a87d55
dean-ferreira <deanferreira20@gmail.com>
2024-03-22 12:22:32 -0500

    Implement Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,44 @@
 // Import components
 import StateTile from '../components/StateTile';
 
+// Import hooks
+import { useState, useEffect } from 'react';
+
+// Import axios
+import axios from 'axios';
+
 function Main() {
-    return (
-        <div className="container page-content">
-            <h2>Top States</h2>
-        </div>
-    );
+    const [states, setStates] = useState([]);
+
+    async function getStates() {
+        try {
+            const response = await axios.get(
+                'http://localhost:3000/states/lowest/3'
+            );
+            setStates(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getStates();
+    }, []);
+
+    const loaded = () => {
+        return (
+            <div className="container page-content">
+                <h2 className="content-title">Top States</h2>
+                <StateTile statePrices={states} />
+            </div>
+        );
+    };
+
+    const loading = () => {
+        return <h2>Loading...</h2>;
+    };
+
+    return states.length ? loaded() : loading();
 }
 
 export default Main;
