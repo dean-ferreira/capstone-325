e22c50aaf9d9d917586076ad4bec2c788ea9b7b1
dean-ferreira <deanferreira20@gmail.com>
2024-03-26 13:54:15 -0500

    Implement add/delete favorite button

--- client/src/components/StateTile.jsx
+++ client/src/components/StateTile.jsx
@@ -3,55 +3,120 @@ import '../App.css';
 // Import components
 import FuelOption from './FuelOption';
 
+// Import hooks
+import { useState, useEffect } from 'react';
+
 // Import axios
 import axios from 'axios';
 
 function StateTile(props) {
-    async function addToFavorites(stateID) {
+    const [user_ID, setUserID] = useState('');
+    const [favorites, setFavorites] = useState([]);
+
+    async function getUser() {
         try {
-            const response = await axios.post('http://localhost:3000/fav', {
-                state_id: stateID,
-                user_id: '6601b4932e23c86172ae1c81',
-            });
-            console.log(response.data);
+            const response = await axios.get('http://localhost:3000/users/');
+            setUserID(response.data[0]._id);
+            getFavorites(response.data[0]._id);
         } catch (error) {
             console.error(error);
         }
     }
-    return (
-        <div className="container page-content">
-            {props.statePrices.map((state, index) => (
-                <div key={index} className="state-tile">
-                    <h2 className="state-name">{state.name}</h2>
-                    <div className="fuel-selection">
-                        <FuelOption
-                            name="Regular"
-                            price={state.gasoline}
-                            grade="87"
-                        />
-                        <FuelOption
-                            name="Mid-Grade"
-                            price={state.midGrade}
-                            grade="89"
-                        />
-                        <FuelOption
-                            name="Premium"
-                            price={state.premium}
-                            grade="91"
-                        />
-                        <FuelOption
-                            name="Diesel"
-                            price={state.diesel}
-                            grade="Diesel"
-                        />
+
+    async function getFavorites(userID) {
+        try {
+            const response = await axios.get(
+                `http://localhost:3000/fav/user/${userID}`
+            );
+            setFavorites(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function addToFavorites(stateID) {
+        try {
+            const response = await axios.post('http://localhost:3000/fav', {
+                state_id: stateID,
+                user_id: user_ID,
+            });
+            setFavorites([...favorites, response.data]);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function removeFromFavorites(favoriteID) {
+        try {
+            const response = await axios.delete(
+                `http://localhost:3000/fav/${favoriteID}`
+            );
+            setFavorites(
+                favorites.filter((favorite) => favorite.state_id !== favoriteID)
+            );
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    function isFavorite(stateID) {
+        return favorites.some((favorite) => favorite.state_id === stateID);
+    }
+
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    const loaded = () => {
+        return (
+            <div className="container page-content">
+                {props.statePrices.map((state, index) => (
+                    <div key={index} className="state-tile">
+                        <h2 className="state-name">{state.name}</h2>
+                        <div className="fuel-selection">
+                            <FuelOption
+                                name="Regular"
+                                price={state.gasoline}
+                                grade="87"
+                            />
+                            <FuelOption
+                                name="Mid-Grade"
+                                price={state.midGrade}
+                                grade="89"
+                            />
+                            <FuelOption
+                                name="Premium"
+                                price={state.premium}
+                                grade="91"
+                            />
+                            <FuelOption
+                                name="Diesel"
+                                price={state.diesel}
+                                grade="Diesel"
+                            />
+                        </div>
+                        {isFavorite(state._id) ? (
+                            <button
+                                onClick={() => removeFromFavorites(state._id)}
+                            >
+                                Remove from Favorites
+                            </button>
+                        ) : (
+                            <button onClick={() => addToFavorites(state._id)}>
+                                Add to Favorites
+                            </button>
+                        )}
                     </div>
-                    <button onClick={() => addToFavorites(state._id)}>
-                        Add to Favorites
-                    </button>
-                </div>
-            ))}
-        </div>
-    );
+                ))}
+            </div>
+        );
+    };
+
+    const loading = () => {
+        return <h2>Loading...</h2>;
+    };
+
+    return props.statePrices.length ? loaded() : loading();
 }
 
 export default StateTile;
