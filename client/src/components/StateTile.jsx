7f56d835d2adf6132c10c37c2d6b16e991a398d4
dean-ferreira <deanferreira20@gmail.com>
2024-03-26 12:18:32 -0500

    Implement  button

--- client/src/components/StateTile.jsx
+++ client/src/components/StateTile.jsx
@@ -3,7 +3,21 @@ import '../App.css';
 // Import components
 import FuelOption from './FuelOption';
 
+// Import axios
+import axios from 'axios';
+
 function StateTile(props) {
+    async function addToFavorites(stateID) {
+        try {
+            const response = await axios.post('http://localhost:3000/fav', {
+                state_id: stateID,
+                user_id: '6601b4932e23c86172ae1c81',
+            });
+            console.log(response.data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
         <div className="container page-content">
             {props.statePrices.map((state, index) => (
@@ -31,6 +45,9 @@ function StateTile(props) {
                             grade="Diesel"
                         />
                     </div>
+                    <button onClick={() => addToFavorites(state._id)}>
+                        Add to Favorites
+                    </button>
                 </div>
             ))}
         </div>
